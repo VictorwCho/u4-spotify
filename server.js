cc8a475f3d48e2484c8f30d33e52d79e34dac48a
Iris <irischoow@gmail.com>
2021-11-22 00:04:32 -0800

    Initial commit deploy server

--- server.js
+++ server.js
@@ -30,10 +30,21 @@ const generateRandomString = (length) => {
   return text;
 };
 
-// connects express and index.html.
-app.use(express.static(__dirname + '/public'))
-.use(cors())
-.use(cookieParser());
+// // connects express and index.html.
+// app.use(express.static(__dirname + '/public'))
+// .use(cors())
+// .use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({message: "OK"});
+});
 
 // adds users into the database.
 app.post('/user', (req, res) => {
