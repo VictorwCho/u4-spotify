0c836d6dd342f171c37bfa0ed93aecdbd2fa104a
Robert Wilson <robertwilson875@gmail.com>
2021-11-28 13:58:58 -0800

    got functionality to add tracks to playlists done

--- public/js/artists.js
+++ public/js/artists.js
@@ -1,83 +1,127 @@
-function searchArtist() {
+async function ensureArtistData() {
   const access_token = localStorage.getItem('access_token');
-  const user_id = '8vnif1sopntbz8ples7srgx88';
   const artistName = document.getElementById('artist_search').value;
   const artistContainer = document.getElementById('artist_container');
 
-  const xhttp = new XMLHttpRequest();
-  const endPoint = `https://api.spotify.com/v1/search?query=${artistName}&offset=0&limit=20&type=artist&market=US`;
+  const artist = await searchArtist(access_token, artistName);
+  const { id: artistId } = artist;
 
-  xhttp.open('GET', endPoint, true);
+  const albums = await getAlbums(access_token, artistId);
 
-  xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  xhttp.setRequestHeader('Accept', 'application/json');
-  xhttp.setRequestHeader('Authorization', `Bearer ${access_token}`);
+  const trackPromises = albums.map(({ id: albumId }) => getTop5AlbumTracks(access_token, albumId));
 
-  xhttp.send();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const response = xhttp.response;
-      // console.log(JSON.stringify(response));
-      console.log(JSON.parse(response));
-      let objectSearched = JSON.parse(response);
-      console.log(objectSearched.artists.items[0].name);
+  const trackResults = await Promise.all(trackPromises);
 
-      for (let i = 0; i < objectSearched.artists.items.length; i++) {
-        if (objectSearched.artists.items[i].images[2]) {
-          artistContainer.innerHTML += `<img src="${objectSearched.artists.items[i].images[2].url}"/>`;
-        } else if (objectSearched.artists.items[i].images[1]) {
-          artistContainer.innerHTML += `<img src="${objectSearched.artists.items[i].images[1].url}"/>`;
-        } else if (objectSearched.artists.items[i].images[0]) {
-          artistContainer.innerHTML += `<img src="${objectSearched.artists.items[i].images[0].url}"/>`;
-        } else {
-          artistContainer.innerHTML += 'SORRY NO IMAGE';
-        }
+  const tracks = trackResults.reduce((prev, curr) => {
+    return [...prev, ...curr];
+  }, []);
 
-        artistContainer.innerHTML += `<br>` + `NAME: ${objectSearched.artists.items[i].name}`;
-        artistContainer.innerHTML += `<br>` + 'GENRES: ' + objectSearched.artists.items[i].genres;
-        artistContainer.innerHTML += `<br>` + 'FOLLOWERS: ' + objectSearched.artists.items[i].followers.total;
-        artistContainer.innerHTML += `<br>` + 'POPULARITY: ' + objectSearched.artists.items[i].popularity;
-        artistContainer.innerHTML += `<br>` + 'LINK: ' + objectSearched.artists.items[i].href;
-        artistContainer.innerHTML += `<br>` + 'ID: ' + objectSearched.artists.items[i].id;
-        artistContainer.innerHTML +=
-          `<br>` + `<a id = "anchors" href = " ${objectSearched.artists.items[i].external_urls.spotify}">Listen to ${objectSearched.artists.items[i].name}</a>`;
-        artistContainer.innerHTML += `<br><br>`;
-      }
-    }
-  };
+  await addToPlaylist(access_token, tracks);
+
+  // ADD STUFF TO DOM
+  if (artist.images[2]) {
+    artistContainer.innerHTML += `<img src="${artist.images[2].url}"/>`;
+  } else if (artist.images[1]) {
+    artistContainer.innerHTML += `<img src="${artist.images[1].url}"/>`;
+  } else if (artist.images[0]) {
+    artistContainer.innerHTML += `<img src="${artist.images[0].url}"/>`;
+  } else {
+    artistContainer.innerHTML += 'SORRY NO IMAGE';
+  }
+
+  artistContainer.innerHTML += `<br>` + `NAME: ${artist.name}`;
+  artistContainer.innerHTML += `<br>` + 'GENRES: ' + artist.genres;
+  artistContainer.innerHTML += `<br>` + 'FOLLOWERS: ' + artist.followers.total;
+  artistContainer.innerHTML += `<br>` + 'POPULARITY: ' + artist.popularity;
+  artistContainer.innerHTML += `<br>` + 'LINK: ' + artist.href;
+  artistContainer.innerHTML += `<br>` + 'ID: ' + artist.id;
+  artistContainer.innerHTML += `<br>` + `<a id = "anchors" href = " ${artist.external_urls.spotify}">Listen to ${artist.name}</a>`;
+  artistContainer.innerHTML += `<br><br>`;
 }
 
 // FIRST GET ARTISTS,
 // `https://api.spotify.com/v1/search?query=${artistName}&offset=0&limit=20&type=artist&market=US`
+async function searchArtist(access_token, artistName) {
+  const url = `https://api.spotify.com/v1/search?query=${artistName}&offset=0&limit=20&type=artist&market=US`;
+
+  const requestConfig = {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Accept: 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+
+  const {
+    data: {
+      artists: { items },
+    },
+  } = await axios.get(url, requestConfig);
+
+  return items[0];
+}
 
 // THEN GET ALBUMS FOR ARTIST,
 // `https://api.spotify.com/v1/artists/ARTIST_ID/albums?country=US&limit=5`
+async function getAlbums(access_token, artistId) {
+  const url = `https://api.spotify.com/v1/artists/${artistId}/albums?country=US&limit=5`;
+
+  const requestConfig = {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Accept: 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+
+  const {
+    data: { items },
+  } = await axios.get(url, requestConfig);
+
+  return items;
+}
 
 // THEN GET TRACKS FROM ALBUM,
 // `https://api.spotify.com/v1/albums/ALBUM_ID/tracks?country=US&limit=5`
+async function getTop5AlbumTracks(access_token, albumId) {
+  const url = `https://api.spotify.com/v1/albums/${albumId}/tracks?country=US&limit=5`;
+
+  const requestConfig = {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Accept: 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+
+  const {
+    data: { items },
+  } = await axios.get(url, requestConfig);
+
+  return items;
+}
 
 // THEN ADD TRACKS TO PLAYLIST.
-// `https://api.spotify.com/v1/playlists/PLAYLIST_ID/tracks?uris=spotify:track:4iV5W9uYEdYUVa79Axb7Rh,spotify:track:1301WleyT98MSxVHPZCA6M`
+// `https://api.spotify.com/v1/playlists/8vnif1sopntbz8ples7srgx88/tracks?uris=spotify:track:4iV5W9uYEdYUVa79Axb7Rh,spotify:track:1301WleyT98MSxVHPZCA6M`
 
-function addToPlaylist() {
-  const access_token = localStorage.getItem('access_token');
-  const xhttp = new XMLHttpRequest();
-  const endPoint = `https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks?country=US&limit=5`;
+async function addToPlaylist(access_token, tracks) {
+  const url = `https://api.spotify.com/v1/playlists/3dRZd09p8aOE8FPLvYUVlF/tracks`;
 
-  xhttp.open('GET', endPoint, true);
-
-  xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  xhttp.setRequestHeader('Accept', 'application/json');
-  xhttp.setRequestHeader('Authorization', `Bearer ${access_token}`);
-
-  xhttp.send();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const response = xhttp.response;
-      console.log(JSON.parse(response));
-      // console.log(JSON.parse(response));
-      // let objectSearched = JSON.parse(response);
-      // console.log(objectSearched.artists.items[0].name);
-    }
+  const requestConfig = {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Accept: 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
   };
+
+  await axios.post(
+    url,
+    {
+      uris: tracks.map(({ id: trackId }) => {
+        return `spotify:track:${trackId}`;
+      }),
+    },
+    requestConfig
+  );
 }
